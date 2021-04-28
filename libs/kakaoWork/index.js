ad04361cb8fadf177047c6ae26d4a71fc428c2c5
정재영 <jjy600901@gmail.com>
2021-04-28 18:23:12 +0000

    Modify kakaoWork library

--- libs/kakaoWork/index.js
+++ libs/kakaoWork/index.js
@@ -1,6 +1,7 @@
 const Config = require('config');
 
 const axios = require('axios');
+const blockBuilder = require('../block_builder');
 const kakaoInstance = axios.create({
 	baseURL : 'https://api.kakaowork.com',
 	headers: {
@@ -8,13 +9,12 @@ const kakaoInstance = axios.create({
 	}
 });
 
-exports.getUserList = async () => {
+getUserList = async () => {
 	const res = await kakaoInstance.get('/v1/users.list');
-	console.log(res);
 	return res.data.users;
 }
 // 채팅방 생성
-exports.openConversations = async({ userId }) => {
+openConversations = async({ userId }) => {
 	const data = {
 		user_id : userId
 	};
@@ -24,7 +24,7 @@ exports.openConversations = async({ userId }) => {
 }
 
 // 메시지 전송
-exports.sendMessage = async ({ conversationId, text, blocks }) => {
+sendMessage = async ({ conversationId, text, blocks }) => {
 	const data = {
 		conversation_id: conversationId,
 		text,
@@ -34,3 +34,23 @@ exports.sendMessage = async ({ conversationId, text, blocks }) => {
 	const res = await kakaoInstance.post('/v1/messages.send', data);
 	return res.data.message;
 }
+
+sendMessageToEveryUsers = async () => {
+	const users = await getUserList();
+	const conversations = await Promise.all(
+		users.map((u) => openConversations({ userId: u.id }))
+	);
+	
+	const messages = await Promise.all([
+		conversations.map((c) => sendMessage(
+			blockBuilder.block_select_menus(c.id)
+		))
+	]);
+	
+	return {users, conversations, messages};
+}
+
+exports.getUserList = getUserList;
+exports.openConversations = openConversations;
+exports.sendMessage = sendMessage;
+exports.sendMessageToEveryUsers = sendMessageToEveryUsers;
